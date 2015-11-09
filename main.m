841722e049c0db33504b7fb648e4646e132a1a4b
alena-kathrin.schnurr <alena-kathrin.schnurr@st.ovgu.de>
2015-11-09 16:59:43 +0100

    3D Viewer

--- main.m
+++ main.m
@@ -1,13 +1,15 @@
 clear all;
 addpath('loadDICOM');
-parentpath = fileread('PathToDataset.txt') % Copy 'PathToDataset.txt.sample' to 'PathToDataset.txt' set the correct path
-dataset = 'p02'
-scan = 't1'
+addpath('Imshow3D');
 
-filepath = strcat(parentpath,'\','Data_v2\',dataset,'\',scan)
+parentpath = fileread('PathToDataset.txt'); % Copy 'PathToDataset.txt.sample' to 'PathToDataset.txt' set the correct path
+dataset = 'p02';
+scan = 't1';
+
+filepath = strcat(parentpath,'\','Data_v2\',dataset,'\',scan);
 
 path = getAllFiles(filepath);
 [names,image3d] = loadDICOM(path);
 
 figure,
-imshow(image3d(:,:,4),[]);
+imshow3D(image3d,[])
