2a994df3e25bf4c0ebd13f1c03ebb61af29fb6b4
NoNoid <NoNoid@gmx-topmail.de>
2015-11-09 16:54:23 +0100

    now able to set path to dataset

--- main.m
+++ main.m
@@ -1,10 +1,10 @@
 clear all;
 addpath('loadDICOM');
-parentpath = cd(cd('..'));
-dataset = 'p02';
-scan = 't1';
+parentpath = fileread('PathToDataset.txt') % Copy 'PathToDataset.txt.sample' to 'PathToDataset.txt' set the correct path
+dataset = 'p02'
+scan = 't1'
 
-filepath = strcat(parentpath,'/','Data_v2/',dataset,'/',scan);
+filepath = strcat(parentpath,'\','Data_v2\',dataset,'\',scan)
 
 path = getAllFiles(filepath);
 [names,image3d] = loadDICOM(path);
