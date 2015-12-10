8b422e72522dde36abc6b3e3ff8c507eb47a9787
gollabaer <bjoern.golla@gmx.de>
2015-12-10 23:15:50 +0100

    added method to convert from anisotropic to isotropic volumes !still needs to be tested though!

---
+++ loadDICOM/anisoToIsotropic.m
@@ -0,0 +1,18 @@
+function [ isotropicVolume ] = anisoToIsotropic( anisotropicVolume, xRes, yRes, zRes) 
+    %get volume dimensions
+    [axDim, ayDim, azDim] = size(anisotropicVolume);
+    %get finest resolution
+    minDim = min([xRes,yRes, zRes]);
+    %get sample ratios per dim
+    Div = minDim./[xRes, yRes, zRes];
+    %get points where to sample
+    samplesX = (1:Div(1):axDim);
+    samplesY = (1:Div(2):ayDim);
+    samplesZ = (1:Div(3):azDim);
+    %get all combinations
+    [Sx, Sy, Sz] = meshgrid(samplesX,samplesY, samplesZ);
+    
+    isotropicVolume = interp3(anisotropicVolume, Sx, Sy, Sz);
+
+end
+
