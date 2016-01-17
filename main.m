48883d183c42b30aa45fc0779f15c9ae54e05f55
Aerl <alena-kathrin@hotmail.de>
2016-01-17 14:22:24 +0100

    smoothing distance field can be turned off

--- main.m
+++ main.m
@@ -2,15 +2,20 @@
 global s;
 s = struct('OriginalImages',{},'ResampledImages',{},'Names',{}, 'Segmentation',{}, 'BinarySegmentation', {});
 global p;
-p = struct('iterations',[],'delta_time',[],'gac_weight',[],'propagation_weight',[],'mu',[],'resolution',{}, 'subsamplingIsOn',[]);
-p(1).iterations = 10;
+p = struct('iterations',[],'delta_time',[],'gac_weight',[],'propagation_weight',...
+    [],'mu',[],'resolution',{}, 'subsamplingIsOn',[], 'smoothDistanceFieldIsOn',[],...
+    'gaussSize',[],'gaussSigma',[]);
+p(1).iterations = 20;
 p(1).delta_time = 0.5;
-p(1).propagation_weight = 5e-3;
+p(1).propagation_weight = 1e-6;
 p(1).gac_weight = 1 - p(1).propagation_weight;
-p(1).mu = 200;
+p(1).mu = 300;
 p(1).subsamplingIsOn = 1;
+p(1).smoothDistanceFieldIsOn = 0;
+p(1).gaussSize = [10 10];
+p(1).gaussSigma = 8;
 
-close all;
+%close all;
 
 % add subfolders
 addpath('loadDICOM');
@@ -51,15 +56,16 @@ for v =1:5
     margin(3) = margin(3) * 2;
     margin = round(margin);
     center = center/2;
-    center(1:2) = center(1:2)*1.02;
+    center(1:2) = center(1:2)*1.1;
     center = round(center);
     
     %initialize distance field
     distance_field = initialize_distance_field(size(image), center, margin, 0.5);
-    
-    % Create the gaussian filter with hsize = [5 5] and sigma = 2
-    %gauss_filter = fspecial('gaussian',[10 10],2);
-    %distance_field = imfilter(distance_field,gauss_filter,'same');
+    % smooth Initialization
+    if (p(1).smoothDistanceFieldIsOn)
+        gauss_filter = fspecial('gaussian',p(1).gaussSize,p(1).gaussSigma);  % size = [5 5] and sigma = 2
+        distance_field = imfilter(distance_field,gauss_filter,'same');
+    end
     
     % segment vertebra using hybrid level set
     [s(1).Segmentation{v}, s(1).BinarySegmentation{v}] = levelSet( image, distance_field, gradient_field, p(1).resolution{v} );
@@ -78,9 +84,9 @@ for v =1:5
     
     % recalculate center of anisotropic data
     if (p(1).subsamplingIsOn)
-        center = size(originalImage);
+        center = size(image);
         center = center/2;
-        center(1:2) = center(1:2)*1.02;
+        center(1:2) = center(1:2)*1.1;
         center = round(center);
     end
     
