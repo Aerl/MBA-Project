4707033cd2b401cece1e9cd6282fe6c95897ceb7
Aerl <alena-kathrin@hotmail.de>
2016-01-17 13:39:07 +0100

    subsampling can be turned off

--- main.m
+++ main.m
@@ -2,12 +2,13 @@
 global s;
 s = struct('OriginalImages',{},'ResampledImages',{},'Names',{}, 'Segmentation',{}, 'BinarySegmentation', {});
 global p;
-p = struct('iterations',[],'delta_time',[],'gac_weight',[],'propagation_weight',[],'mu',[],'resolution',{});
+p = struct('iterations',[],'delta_time',[],'gac_weight',[],'propagation_weight',[],'mu',[],'resolution',{}, 'subsamplingIsOn',[]);
 p(1).iterations = 10;
 p(1).delta_time = 0.5;
 p(1).propagation_weight = 5e-3;
 p(1).gac_weight = 1 - p(1).propagation_weight;
 p(1).mu = 200;
+p(1).subsamplingIsOn = 0;
 
 close all;
 
@@ -31,15 +32,20 @@ filepath = strcat(parentpath,'\','Data_v2\',dataset,'\',scan);
 path = getAllFiles(filepath);
 [s(1).Names,s(1).ResampledImages,s(1).OriginalImages] = loadDICOM(path);
 
+%% Segmentation of all vertebrae
 for v =1:5
     
-    
     % select Vertebra
-    image = s(1).ResampledImages{v};
+    if (p(1).subsamplingIsOn)
+        image = s(1).ResampledImages{v};
+    else
+        image = s(1).OriginalImages{v};
+    end
     
-    %% Hybrid 3D Levelset
-    % g = ones(size(V)); % linear diffusion
+    % compute gradient field
     gradient_field = ac_gradient_map(image,1);
+    
+    % set center and margin depending on image size
     center = size(image);
     margin = center * 0.08;
     margin(3) = margin(3) * 2;
@@ -48,72 +54,48 @@ for v =1:5
     center(1:2) = center(1:2)*1.02;
     center = round(center);
     
+    %initialize distance field
     distance_field = initialize_distance_field(size(image), center, margin, 0.5);
     
     % Create the gaussian filter with hsize = [5 5] and sigma = 2
     %gauss_filter = fspecial('gaussian',[10 10],2);
     %distance_field = imfilter(distance_field,gauss_filter,'same');
     
+    % segment vertebra using hybrid level set
     [s(1).Segmentation{v}, s(1).BinarySegmentation{v}] = levelSet( image, distance_field, gradient_field, p(1).resolution{v} );
     
-    % result = cell(size(image,3),1);
-    % for i = 1:size(image,3)
-    %     result{i} = contours(distance_field(:,:,i),[0,0]);
-    % end
-    
-    image = s(1).OriginalImages{v};
-    
+    % show segmentation
+    originalImage = s(1).OriginalImages{v};
     figure;
     slice = 1:15;
     for i = 1:length(slice)
-        subplot(3,5,i); imshow(image(:,:,slice(i)),[]); hold on;
+        subplot(3,5,i); imshow(originalImage(:,:,slice(i)),[]); hold on;
         r = s(1).Segmentation{v}{slice(i)};
         if ~isempty(r)
             [h, pt] = zy_plot_contours(r,'linewidth',2);
         end
     end
-
+    
     % recalculate center of anisotropic data
-    center = size(s(1).OriginalImages{v});
-    center = center/2;
-    center(1:2) = center(1:2)*1.02;
-    center = round(center);
-
+    if (p(1).subsamplingIsOn)
+        center = size(originalImage);
+        center = center/2;
+        center(1:2) = center(1:2)*1.02;
+        center = round(center);
+    end
+    
     % connected component analysis
     l = bwlabeln(s(1).BinarySegmentation{v});
     labelOfVertebra = l(center(1),center(2),center(3));
-    
     binaryResult = (l==labelOfVertebra);
     
+    % show binary verterbra segmentation
     figure;
     for i = 1:length(slice)
         subplot(3,5,i); imshow(binaryResult(:,:,slice(i)),[]);
     end
     
 end
-
-%% Chan Vese 3D Levelset
-% smooth_weight = 3;
-% image_weight = 1e-4 ;
-% delta_t = 1;
-%
-% margin = 30;
-% phi = zeros(size(img));
-% phi(margin:end-margin, margin:end-margin, 5:end-5) = 1;
-% phi = ac_reinit(phi-0.5);
-%
-% for i = 1:10
-%     phi = ac_ChanVese_model(img, phi, smooth_weight, image_weight, delta_t, 1);
-% end
-%
-% figure;
-% slice = 29:41;
-% for i = 1:numel(slice)
-%     subplot(3,4,i); imshow(img(:,:,slice(i)),[]); hold on;
-%     c = contours(phi(:,:,slice(i)),[0,0]);
-%     zy_plot_contours(c,'linewidth',2);
-% end
-
 
 % clear workspace
 clear all;
