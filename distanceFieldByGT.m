b673b7dd4a4b105f96b7e8073df07297771c7151
bjoern.golla <bjoern.golla@st.ovgu.de>
2016-01-23 16:04:04 +0100

    bugfix

--- distanceFieldByGT.m
+++ distanceFieldByGT.m
@@ -1,28 +1,29 @@
-function [ distanceField] = distanceFieldByGT(BoxMIN, BoxDim, vertibrae, size)
+function [ distanceField] = distanceFieldByGT(BoxMIN, BoxDim, vertibrae, dfsize)
     
     EROSION = 1;
     
-    GT = loadEroadedGT(vertibrae, EROSION);
-    [XGT, YGT, ZGT] = ind2sub(sizeGT, find(GT));
+    GT = loadErodedGT(vertibrae, EROSION);
+    GT = im2double(GT);
+    [XGT, YGT, ZGT] = ind2sub(size(GT), find(GT));
     
     BoundingMIN = [min(XGT), min(YGT), min(ZGT)];
     BoundingMAX = [max(XGT), max(YGT), max(ZGT)];
     BoundDim = BoundingMAX - BoundingMIN;
-    BoxMAX = BoxMIN + BoxDIM;
+    BoxMAX = BoxMIN + BoxDim;
     
-    distanceField = zeros(size);
+    distanceField = zeros(dfsize);
     
     % Copy values from Bounding in GT to Box in Levelset
     
-    sampx = (BoundingMIN(1): BoundDim(1) / BoxDim(1) : BoundindMAX(1));
-    sampy = (BoundingMIN(2): BoundDim(2) / BoxDim(2) : BoundindMAX(2));
-    sampz = (BoundingMIN(3): BoundDim(3) / BoxDim(3) : BoundindMAX(3));
+    sampx = linspace(BoundingMIN(1), BoundingMAX(1), BoxDim(1)+1);
+    sampy = linspace(BoundingMIN(2), BoundingMAX(2), BoxDim(2)+1);
+    sampz = linspace(BoundingMIN(3), BoundingMAX(3), BoxDim(3)+1);
     
     
     
     [Sx, Sy, Sz] = meshgrid(sampx, sampy, sampz);
     
-    distanceField(boxMIN(1):boxMAX(1), boxMIN(2):boxMAX(2), boxMIN(3):boxMAX(3)) ...
+    distanceField(BoxMIN(1):BoxMAX(1), BoxMIN(2):BoxMAX(2), BoxMIN(3):BoxMAX(3)) ...
         = interp3(GT, Sx, Sy, Sz, 'cubic');
     
 
